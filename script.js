47a863fcc793d6921229fec08056a14faf48b318
nihatran <89045243+nihatran@users.noreply.github.com>
2023-12-26 17:04:25 -0500

    Add active class to buttons and instructions

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
-function createGrid(size = 16, className = 'grid-16x16') {
+function removeActiveButton(buttons) {
+    buttons.forEach((button) => {
+        button.classList.remove('active-button')
+    })
+}
+
+function createGrid(size, className) {
     let container = document.querySelector('.container')
     container.innerHTML = '';
     for(let i = 0; i < size*size; i++) {
@@ -13,13 +19,19 @@ function createGrid(size = 16, className = 'grid-16x16') {
 
 function chooseGrid() {
     let sizeButtons = document.querySelectorAll('.size-buttons button');
+    let colorButtons = document.querySelectorAll('.color-buttons button');
     sizeButtons.forEach((button) => {
         button.addEventListener('click', () => {
+            removeActiveButton(sizeButtons);
+            removeActiveButton(colorButtons);
             if (button.classList.contains('small')) {
+                sizeButtons[0].classList.add('active-button');
                 createGrid(16, 'grid-16x16');
             } else if (button.classList.contains('medium')) {
+                sizeButtons[1].classList.add('active-button');
                 createGrid(32, 'grid-32x32');
             } else if (button.classList.contains('large')) {
+                sizeButtons[2].classList.add('active-button');
                 createGrid(64, 'grid-64x64');
             }
         });
@@ -46,21 +58,26 @@ function chooseColor() {
     let colorButtons = document.querySelectorAll('.color-buttons button');
     colorButtons.forEach((button) => {
         button.addEventListener('click', () => {
+            removeActiveButton(colorButtons);
             if (button.classList.contains('black')) {
+                colorButtons[0].classList.add('active-button');
                 drawColor('black', '#000000');
             } else if (button.classList.contains('white')) {
                 drawColor('white', '#FFFFFF');
+                colorButtons[1].classList.add('active-button');
             } else if (button.classList.contains('rainbow')) {
                 let randomColors = [];
                 for(let i = 0; i < 255; i++) {
                     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
                     randomColors.push(randomColor);
                 }
+                colorButtons[2].classList.add('active-button');
                 drawColor('rainbow', randomColors);
             }
         });
     });
 }
+
 
 chooseGrid();
 createGrid();
